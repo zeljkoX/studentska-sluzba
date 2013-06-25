e7b88747abb6f6959be2b5a16a67e4d071ab1fb0
Zeljko Markovic <zeljko89markovic@gmail.com>
2013-06-25 17:36:27 +0200

    Backbone direktorijum dodan

--- administracija/routes/studenti.js
+++ administracija/routes/studenti.js
@@ -2,7 +2,7 @@ module.exports = function(app, model){
 
 app.get('/student',function(req, res){
 	console.log('pozdrav korisnice');
-	var a = new model({index:'3243423', licniPodaci:{ime:'Zeljko'}});
+	model.Student.create({index:'3243423', licniPodaci:{ime:'Zeljko'}});
 	a.save(function(){});
 	res.send(200);
 });
